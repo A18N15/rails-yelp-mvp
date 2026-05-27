2c967bebbab8392eb116a49ffd16dc17ce6d1f11
issanode <atognide@pm.me>
2026-05-27 16:54:53 -0400

    t

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Creating restaurants..."
+kitano = Restaurant.new(name: "Kitano", address: "Rachel et Coloniale", phone_number: "51455555555", category: "japanese")
+kitano.save!
+
+steakhouse = Restaurant.new(name: "Steakhouse", address: "Prince Arthur et De Bullion", phone_number: "51455555556", category: "french")
+steakhouse.save!
+
+sushi_express = Restaurant.new(name: "Sushi express", address: "666 St-Laurent", phone_number: "51455555557", category: "japanese")
+sushi_express.save!
+
+mon_nan = Restaurant.new(name: "Mon nan", address: "Chinatown", phone_number: "51455555558", category: "chinese")
+mon_nan.save!
+
+chez_claudette = Restaurant.new(name: "Chez claudette", address: "Vieux Laurier", phone_number: "51455555558", category: "french")
+chez_claudette.save!
